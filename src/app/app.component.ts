1eaead0db2d179dc2be4d65993f0191dc4e90561
riderprathap <prathap1410@gmail.com>
2023-08-13 11:10:04 +0530

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, ViewChild } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Avengers';
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngOnInit(): void {
+    const audioElement = this.el.nativeElement.querySelector('audio');
+
+    if (audioElement) {
+      this.renderer.listen(audioElement, 'canplaythrough', () => {
+        audioElement.volume = 0.40;
+      });
+    }
+  }
 }
